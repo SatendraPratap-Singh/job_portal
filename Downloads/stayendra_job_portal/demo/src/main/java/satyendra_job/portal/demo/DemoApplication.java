a6f5530151c81b513cb4bf0e2af9123501e13145
Sneha <snehapal9584@gmail.com>
2026-04-03 02:43:40 +0530

    final fix controller structure

--- Downloads/stayendra_job_portal/demo/src/main/java/satyendra_job/portal/demo/DemoApplication.java
+++ Downloads/stayendra_job_portal/demo/src/main/java/satyendra_job/portal/demo/DemoApplication.java
@@ -3,12 +3,10 @@ package satyendra_job.portal.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "satyendra_job.portal.demo")
+@SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
