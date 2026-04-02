6dfe614c66ca353c6e1b6a58b21e39daf813e1ef
Sneha <snehapal9584@gmail.com>
2026-04-03 02:10:20 +0530

    fix component scan

--- Downloads/stayendra_job_portal/demo/src/main/java/satyendra_job/portal/demo/DemoApplication.java
+++ Downloads/stayendra_job_portal/demo/src/main/java/satyendra_job/portal/demo/DemoApplication.java
@@ -3,7 +3,7 @@ package satyendra_job.portal.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "satyendra_job.portal.demo")
 public class DemoApplication {
 
 	public static void main(String[] args) {
